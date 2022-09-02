2ea2bd08ba5fc45e04ef694b10a0efadaa440b4f
nipunsperera <samadinipun@gmail.com>
2022-09-02 13:39:46 +0530

    Implement validation logic for the login form

--- src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
@@ -3,12 +3,38 @@ package lk.ijse.dep9.clinic.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 
 public class LoginFormController {
     public JFXTextField txtUsername;
     public JFXTextField txtPassword;
     public JFXButton btnLogin;
 
+    public void initialize(){
+        btnLogin.setDefaultButton(true);
+
+    }
+
     public void btnLoginOnAction(ActionEvent actionEvent) {
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+
+        if(username.isBlank()){
+            new Alert(Alert.AlertType.ERROR,"Username is empty").showAndWait();
+            txtUsername.requestFocus();
+            txtUsername.selectAll();
+            return;
+        }else if (username.isBlank()) {
+            new Alert(Alert.AlertType.ERROR, "Password is empty").showAndWait();
+            txtPassword.requestFocus();
+            txtPassword.selectAll();
+            return;
+        }else if(!username.matches("^[A-z0-9]+$")){
+            new Alert(Alert.AlertType.ERROR,"Username is wrong").showAndWait();
+            txtUsername.requestFocus();
+            txtUsername.selectAll();
+            return;
+
+        }
     }
 }
