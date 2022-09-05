1740bd395f5f3011b9baad6604b275eac054b54c
nipunsperera <samadinipun@gmail.com>
2022-09-05 16:50:22 +0530

    Cloes #7

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
@@ -1,5 +1,24 @@
 package lk.ijse.dep9.clinic.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+
 public class AdminDashboardFormController {
 
+    public JFXButton btnProfileManagement;
+    public JFXButton btnViewRecords;
+    public JFXButton btnSettings;
+    public JFXButton btnLogin;
+
+    public void btnProfileManagementOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnViewRecordsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSettingsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
 }
