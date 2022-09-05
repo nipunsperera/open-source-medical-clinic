7b96a7b8b7af1b40ee4c1cb6e873ae711db41e40
nipunsperera <samadinipun@gmail.com>
2022-09-05 17:15:23 +0530

    Close #8

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminSettingFormController.java
@@ -0,0 +1,33 @@
+package lk.ijse.dep9.clinic.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminSettingFormController {
+    public JFXButton btnBack;
+    public JFXButton btnHospitalFee;
+    public JFXButton btnDiscount;
+    public JFXButton btnManageFields;
+    public JFXButton btnPassword;
+    public JFXButton btnAbout;
+    public AnchorPane apScene;
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnHospitalFeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDiscountOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnManageFieldsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAboutOnAction(ActionEvent actionEvent) {
+    }
+}
